766dddbeb293126e756b0046be48e3f05ca75a7a
Tanmay Basu <mailtanmaybasu@gmail.com>
2021-03-10 18:51:21 +0000

    Update testing_data_extraction.py

--- testing_data_extraction.py
+++ testing_data_extraction.py
@@ -6,15 +6,12 @@ Created on Monday December 21 2020 at 18:42:24
 @author: Tanmay Basu
 """
 
-#from data_extraction_tfidf import data_extraction
 from data_extraction import data_extraction
 
+#clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='keyword_matching')
+clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='tfidf',clf_opt='s',no_of_selected_terms=1000)
+#clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='logentropy',clf_opt='s',no_of_selected_terms=1500)
+#clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='doc2vec',clf_opt='s',no_of_selected_terms=1000)
 
-#clf=sentence_classification('/Users/basut/geometric_error_extraction/code/',model='keyword_matching')
-clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='tfidf',clf_opt='s')
-#clf=sentence_classification('/Users/basut/geometric_error_extraction/code/',model='logentropy',clf_opt='s',no_of_selected_terms=1500)
-#clf=sentence_classification('/Users/basut/geometric_error_extraction/code/',model='doc2vec',clf_opt='s',no_of_selected_terms=1000)
-
-#clf.build_training_data()
+clf.build_training_data()
 clf.sentence_classification()
-
