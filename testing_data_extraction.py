a2d9d04d773361337c53d17f635dfb6e26412960
Tanmay Basu <mailtanmaybasu@gmail.com>
2021-03-10 03:37:02 +0000

    Update testing_data_extraction.py

--- testing_data_extraction.py
+++ testing_data_extraction.py
@@ -7,13 +7,14 @@ Created on Monday December 21 2020 at 18:42:24
 """
 
 #from data_extraction_tfidf import data_extraction
-from sentence_classification import sentence_classification
+from data_extraction import data_extraction
 
 
 #clf=sentence_classification('/Users/basut/geometric_error_extraction/code/',model='keyword_matching')
-clf=sentence_classification('/Users/basut/geometric_error_extraction/code/',model='tfidf',clf_opt='s')
+clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='tfidf',clf_opt='s')
 #clf=sentence_classification('/Users/basut/geometric_error_extraction/code/',model='logentropy',clf_opt='s',no_of_selected_terms=1500)
 #clf=sentence_classification('/Users/basut/geometric_error_extraction/code/',model='doc2vec',clf_opt='s',no_of_selected_terms=1000)
 
 #clf.build_training_data()
 clf.sentence_classification()
+
