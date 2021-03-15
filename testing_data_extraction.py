27e0162c524d2d928750d42a66d01b4df8167492
Tanmay Basu <mailtanmaybasu@gmail.com>
2021-03-15 23:05:19 +0000

    Update testing_data_extraction.py

--- testing_data_extraction.py
+++ testing_data_extraction.py
@@ -8,12 +8,11 @@ Created on Monday December 21 2020 at 18:42:24
 
 from data_extraction import data_extraction
 
-
-#clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='keyword_matching')
-#clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='tfidf',clf_opt='s',no_of_selected_terms=1000)
-#clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='logentropy',clf_opt='s',no_of_selected_terms=1500)
-#clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='doc2vec',clf_opt='s',no_of_selected_terms=1000)
-clf=data_extraction('/Users/basut/geometric_error_extraction/code/',model='biobert')
+#clf=data_extraction('/home/xyz/data_extraction/',model='biobert')
+#clf=data_extraction('/home/xyz/data_extraction/',model='keyword_matching')
+#clf=data_extraction('/home/xyz/data_extraction/',model='tfidf',clf_opt='s',no_of_selected_terms=1000)
+clf=data_extraction('/home/xyz/data_extraction/',model='entropy',clf_opt='s',no_of_selected_terms=1500)
+#clf=data_extraction('/home/xyz/data_extraction/',model='doc2vec',clf_opt='s',no_of_selected_terms=1000)
 
 clf.build_training_data()
 clf.sentence_classification()
