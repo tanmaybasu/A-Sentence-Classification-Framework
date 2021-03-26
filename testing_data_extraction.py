6ce2ef4026e203073ff38d4012090e11ac1679df
Tanmay Basu <mailtanmaybasu@gmail.com>
2021-03-26 01:11:25 +0000

    Update testing_data_extraction.py

--- testing_data_extraction.py
+++ testing_data_extraction.py
@@ -8,7 +8,7 @@ Created on Monday December 21 2020 at 18:42:24
 
 from data_extraction import data_extraction
 
-#clf=data_extraction('/home/xyz/data_extraction/',model='biobert',model_source='monologg/biobert_v1.1_pubmed',threshold=0.5)
+#clf=data_extraction('/home/xyz/data_extraction/',model='bert',model_source='monologg/biobert_v1.1_pubmed',threshold=0.5)
 #clf=data_extraction('/home/xyz/data_extraction/',model='keyword_matching',threshold=0.5)
 #clf=data_extraction('/home/xyz/data_extraction/',model='tfidf',clf_opt='s',no_of_selected_terms=1500,threshold=0.5)
 clf=data_extraction('/home/xyz/data_extraction/',model='entropy',clf_opt='s',no_of_selected_terms=1500,threshold=0.5)
