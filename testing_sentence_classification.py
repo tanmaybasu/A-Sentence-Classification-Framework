2802a5b906e0a7cff0a1166f270202a103d200ae
Tanmay Basu <mailtanmaybasu@gmail.com>
2022-03-08 14:31:24 +0530

    Add files via upload

---
+++ testing_sentence_classification.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Monday December 21 2020 at 18:42:24
+
+@author: Tanmay Basu
+"""
+
+from sentence_classification import sentence_classification
+
+
+#clf=sentence_classification('/home/xyz/sentence_classification/',model='keyword_matching',threshold=0.5)
+#clf=sentence_classification('/home/xyz/sentence_classification/',model='tfidf',clf_opt='s',no_of_selected_terms=1000,threshold=0.5)
+clf=sentence_classification('/home/xyz/sentence_classification/',model='entropy',clf_opt='s',no_of_selected_terms=1500,threshold=0.5)
+#clf=sentence_classification('/home/xyz/sentence_classification/',model='doc2vec',vec_len=20,clf_opt='s',no_of_selected_terms=1000,threshold=0.5)
+#clf=sentence_classification('/home/xyz/sentence_classification/',model='bert',model_source='monologg/biobert_v1.1_pubmed',threshold=0.5)
+
+#clf.build_training_data()
+clf.sentence_classification()
